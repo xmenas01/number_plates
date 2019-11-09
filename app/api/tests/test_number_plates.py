d23e3f0e292e7f087394b0f560d34ed36f6d59c0
Vytautas <xmenas@gmail.com>
2019-11-09 22:55:09 +0200

    updated tests

--- app/api/tests/test_number_plates.py
+++ app/api/tests/test_number_plates.py
@@ -4,7 +4,7 @@ from rest_framework.reverse import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
-from api.models import NumberPlate
+from api.models import NumberPlate, CarModel
 
 
 PLATE_URL = reverse('numberplate-list')
@@ -22,24 +22,38 @@ class NumberPlateTest(TestCase):
             }
         }
 
+    def prepopulate_db(self):
+        car_model_rec = {
+            'manufacturer': 'porsche',
+            'model': '911'
+        }
+        car_model = CarModel.objects.create(**car_model_rec)
+        number_plate_rec = {
+            'number' : 'ANJ519',
+            'owner' : 'Peter',
+            'car_model' : car_model
+        }
+        NumberPlate(**number_plate_rec).save()
+
     def test_create_number_plate(self):
         """ Test creation of number plate record with valid payload is successful""" 
-        resp = self.client.post(PLATE_URL, self.payload)
+        resp = self.client.post(PLATE_URL, self.payload, format='json')
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        car_model = CarModel.objects.get(**resp.data.get('car_model'))
+        resp.data['car_model'] = car_model
         self.assertTrue(NumberPlate.objects.filter(**resp.data).exists())
 
     def test_number_plates_duplicaton(self):
         """ Test creation of number plate that already exists, fails """
-        NumberPlate(**self.payload).save()
-        resp = self.client.post(PLATE_URL, self.payload)
+        self.prepopulate_db()
+        resp = self.client.post(PLATE_URL, self.payload, format='json')
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_number_plate_validation(self):
         """ Test creation of number plate format, fails"""
         payload = {
             'number' : 'ANJC519',
-            'owner' : 'Peter',
-            'car_model' : 'porsche 911'
+            'owner' : 'Peter'
         }
         resp = self.client.post(PLATE_URL, payload)
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
